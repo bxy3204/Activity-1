402231c76f685e8f2b6ffe5268ad17b8d951ce9f
dfg9279 <dfg9279@cs.rit.edu>
2017-09-20 08:23:45 -0700

    Added inventory test stubs

---
+++ CoffeeMaker_JUnit/unittests/edu/ncsu/csc326/coffeemaker/InventoryTest.java
@@ -0,0 +1,75 @@
+package edu.ncsu.csc326.coffeemaker;
+
+import junit.framework.TestCase;
+
+public class InventoryTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testInventory() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetChocolate() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetChocolate() {
+		fail("Not yet implemented");
+	}
+
+	public void testAddChocolate() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCoffee() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetCoffee() {
+		fail("Not yet implemented");
+	}
+
+	public void testAddCoffee() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetMilk() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetMilk() {
+		fail("Not yet implemented");
+	}
+
+	public void testAddMilk() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetSugar() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetSugar() {
+		fail("Not yet implemented");
+	}
+
+	public void testAddSugar() {
+		fail("Not yet implemented");
+	}
+
+	public void testEnoughIngredients() {
+		fail("Not yet implemented");
+	}
+
+	public void testUseIngredients() {
+		fail("Not yet implemented");
+	}
+
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
